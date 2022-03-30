96ec3a21f4ea23300851832ba301955162b1efbe
Charlie Hölscher <charlieholscher@gmail.com>
2022-03-30 15:23:00 +0200

    iteration 2

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import 'antd/dist/antd.css';
 import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import foods from './foods.json';
 import { useState } from 'react';
+import FoodBox from './Components/FoodBox';
 
 function App() {
   const [allFoods, setAllFoods] = useState(foods);
 
   return (
     <div className="App">
-      {allFoods.map((food) => {
+      {allFoods.map((singleFood) => {
         return (
           <div>
-            <p>{food.name}</p>
-            <img src={food.image} alt="food" style={{width: '300px'}}/>
+            <p>{singleFood.name}</p>
+            <img src={singleFood.image} alt="food" style={{width: '300px'}}/>
+            {/* <FoodBox food = {singleFood}/> */}
           </div>
         );
       })}
+      
+      <FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+      }}/>
     </div>
-  );
-}
+ 
+  )}
 
 export default App;
